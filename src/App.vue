281ff8d66c89d191876b00ccf1a0f9c2519fb19f
Anthony Benitez <anthonybenixtez@gmail.com>
2024-09-25 20:01:26 -0600

    navbar
    dd

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,19 @@
   <div class="flex flex-col h-screen">
     <!-- Header -->
     <header class="flex items-center h-14 px-4 border-b border-gray-300 sm:h-16 md:px-6 lg:px-8" v-if="!isLoginPage">
-      <div>
+      <div class="flex-1">
         <a class="flex items-center gap-2 font-semibold" href="#">
           <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
         </a>
       </div>
-      <div>
-    <Navbar v-if="!isLoginPage" /> <!-- Renderiza el Navbar solo si no estás en la página de inicio de sesión -->
-    <router-view />
-  </div>
+      <!-- Navbar separado para evitar problemas con el RouterView -->
+      <Navbar v-if="!isLoginPage" />
     </header>
     <!-- Fin Header -->
 
     <!-- Main -->
-    <main>
-      <RouterView />
+    <main class="flex-1 overflow-auto">
+      <router-view />
     </main>
     <!-- Fin Main -->
 
@@ -38,7 +36,6 @@ import Navbar from './modules/landing/pages/Nav.vue'; // Ajusta la ruta según s
 const route = useRoute();
 const isLoginPage = computed(() => route.name === 'PrincipalPagina'); // Cambia 'PrincipalPagina' si es necesario
 </script>
-
 
 <style scoped>
 /* Estilos generales */
